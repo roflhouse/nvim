d3facb8c15ebfcd0346324a3f3413f2323fb26ea
Nick Feeney <nick@rfx.com>
2024-04-13 21:33:42 -0700

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,11 +2,28 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
-    config = function()
-        vim.keymap.set("n", "<Leader>e", ":Neotree toggle<CR>", {}) 
+
+    deactivate = function()
+        vim.cmd([[Neotree close]])
+    end,
+    init = function()
+        if vim.fn.argc(-1) == 1 then
+            local stat = vim.uv.fs_stat(vim.fn.argv(0))
+            if stat and stat.type == "directory" then
+                require("neo-tree")
+            end
+        end
+    end,
+    opts = {
+        close_if_last_window = true,
+        enable_git_status = true,
+    },
+    config = function(_, opts)
+        vim.keymap.set("n", "<Leader>E", ":Neotree toggle<CR>", {desc = "NeoTree"})
+        require("neo-tree").setup(opts)
     end
 }
